b9d6fdf15793c13e35f2b5f21a024d581280c793
Prashant <prashantyallatti79968@gmail.com>
2023-08-19 11:36:00 +0530

    modify

--- server/controllers/foodData_controller.js
+++ server/controllers/foodData_controller.js
@@ -1,6 +1,6 @@
 module.exports.getFoodData = (req, res) => {
   try {
-    res.status(200).json([global.food_items, global.food_categories]);
+    res.send([global.food_items, global.food_categories]);
   } catch (err) {
     // Handle the error appropriately
     console.error(err);
